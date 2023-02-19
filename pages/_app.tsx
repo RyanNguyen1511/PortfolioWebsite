24e9a15a294eed01592cc11033082f98fcd2f6be
RyanNguyen1511 <nguyenminhtuan1511@gmail.com>
2023-02-19 02:06:05 -0800

    fix bug for the npm run build, move AOS remove AOS  manually include

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import AOS from "aos";
 
 export default function App({ Component, pageProps }: AppProps) {
-  
+  useEffect(() => {
+    // here you can add your aos options
+    AOS.init({
+      offset: 100,
+    });
+  }, []);
   return <Component {...pageProps} />
 }
